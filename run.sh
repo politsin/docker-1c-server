b3c3a211e3fda40cb29f61e68c5471cf3bc703e5
Anatoly <politsin@gmail.com>
2017-07-11 10:13:04 +0300

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker run --name 1c-server \
-  --net host \
-  --detach \
-  --volume 1c-server-home:/home/usr1cv8 \
-  --volume 1c-server-logs:/var/log/1C \
-  --volume /etc/localtime:/etc/localtime:ro \
-  alexanderfefelov/1c-server
